7552da5da2269cb3a0bed257eea288d5388e399d
Ksenia <ksenia-2006@yandex.ru>
2026-06-10 19:16:46 +0700

    feat: add DesignTimeDbContextFactory for migrations

--- backend/Eltorto/Eltorto.Infrastructure/Data/AppDbContext.cs
+++ backend/Eltorto/Eltorto.Infrastructure/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Eltorto.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Eltorto.Infrastructure.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
