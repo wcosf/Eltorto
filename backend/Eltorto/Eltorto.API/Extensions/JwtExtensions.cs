98a159e03b93c0132323bcec3ceb435a0d375e06
Ksenia <ksenia-2006@yandex.ru>
2026-06-16 18:39:08 +0700

    refactor: split Program.cs into extension methods
    - Move service registration to Application.DependencyInjection
    - Add SwaggerExtensions, IdentityExtensions, JwtExtensions in API
    - Simplify Program.cs to composition of extension methods

---
+++ backend/Eltorto/Eltorto.API/Extensions/JwtExtensions.cs
@@ -0,0 +1,40 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Eltorto.API.Extensions;
+
+public static class JwtExtensions
+{
+    public static IServiceCollection AddJwtAuthentication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var jwtSettings = configuration.GetSection("JwtSettings");
+        var secretKey = Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings["Issuer"],
+                ValidAudience = jwtSettings["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(secretKey)
+            };
+        });
+
+        services.AddAuthorization();
+
+        return services;
+    }
+}
