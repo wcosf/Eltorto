027fc068996f02dc1199bf8bbfde3e8740d981ce
Ksenia <ksenia-2006@yandex.ru>
2026-06-16 18:39:08 +0700

    refactor: split Program.cs into extension methods
    - Move service registration to Application.DependencyInjection
    - Add SwaggerExtensions, IdentityExtensions, JwtExtensions in API
    - Simplify Program.cs to composition of extension methods

---
+++ backend/Eltorto/Eltorto.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+using Eltorto.Application.Interfaces.Services;
+using Eltorto.Application.Mapping;
+using Eltorto.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eltorto.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<ICakeService, CakeService>();
+        services.AddScoped<IFillingService, FillingService>();
+        services.AddScoped<ITestimonialService, TestimonialService>();
+        services.AddScoped<IOrderService, OrderService>();
+        services.AddScoped<IPageService, PageService>();
+        services.AddScoped<ISliderService, SliderService>();
+        services.AddScoped<IContactSettingsService, ContactSettingsService>();
+        services.AddScoped<IAuthService, AuthService>();
+
+        return services;
+    }
+}
